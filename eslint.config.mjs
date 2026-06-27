2940358529f9f53f4ff86468326a7ae755c4d578
NoobStyle <mikledanse@mail.ru>
2026-06-27 12:17:58 +0200

    added packeges

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+];
