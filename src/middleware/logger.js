9b41243ef2602e8b4449be79328897b6cc5201bc
NoobStyle <mikledanse@mail.ru>
2026-07-04 10:51:26 +0200

    init

---
+++ src/middleware/logger.js
@@ -0,0 +1,8 @@
+import pinoHttp from 'pino-http';
+
+export const logger = pinoHttp({
+  transport:
+    process.env.NODE_ENV !== 'production'
+      ? { target: 'pino-pretty' }
+      : undefined,
+});
--- src/server.js
+++ src/server.js
@@ -1,47 +1,32 @@
 import express from 'express';
 import cors from 'cors';
-import pinoHttp from 'pino-http';
 import dotenv from 'dotenv';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRouter from './routes/notesRoutes.js';
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const startServer = async () => {
+  await connectMongoDB();
 
-app.use(cors());
-app.use(express.json());
+  const app = express();
+  const PORT = process.env.PORT || 3000;
 
-app.use(pinoHttp());
+  app.use(logger);
+  app.use(cors());
+  app.use(express.json());
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    message: 'Retrieved all notes',
+  app.use(notesRouter);
+
+  app.use(notFoundHandler);
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
   });
-});
+};
 
-app.get('/notes/:noteId', (req, res) => {
-  const idParam = req.params.noteId;
-  res.status(200).json({
-    message: `Retrieved note with ID: ${idParam}`,
-  });
-});
-
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
-
-app.use((req, res, next) => {
-  res.status(404).json({
-    message: 'Route not found',
-  });
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: err.message || 'Internal Server Error',
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+startServer();
